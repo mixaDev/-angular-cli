70a92bade28710f4a09b529544273f89030949c3
Михайло Голубовський <lamerumiata@gmail.com>
2017-01-24 12:43:27 +0200

    add list1 list2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { List1Component } from './list1/list1.component';
+import { List2Component } from './list2/list2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    List1Component,
+    List2Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'list1', pathMatch: 'full'},
+      { path: 'list1', component: List1Component},
+      { path: 'list2', component: List2Component}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
